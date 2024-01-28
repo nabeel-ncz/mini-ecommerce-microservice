e6a983a64961bc13bc61871232b2567f225541db
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-01-28 10:58:16 +0530

    patch update, released v1.0.1

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,3 +7,6 @@ export * from "./errors/not-found-error";
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
+
+export * from "./messages/messages";
+export * from "./messages/topics";
---
+++ common/src/messages/messages.ts
@@ -0,0 +1,4 @@
+export const USER_CREATED_MESSAGE = "userCreated";
+export const USER_UPDATED_MESSAGE = "userUpdated";
+export const PRODUCT_CREATED_MESSAGE = "productCreated";
+export const PRODUCT_UPDATED_MESSAGE = "productUpdated";
